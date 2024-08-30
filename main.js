6a26cbeafa5997dba6e05e079a7aa2aa2dff08f3
ScottNovakowski <snovakow@gmail.com>
2024-08-30 17:21:48 -0400

    0.3.0

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const src = document.currentScript.src;
 import("../snovakow/main.js").then(
 	main => {
 		const gtag = (window.location.host === "snovakow.com") ? 'G-DV1KLMY93N' : false;
-		main.initialize(src, gtag).then(() => import("./app.js"));
+		const options = { main: src, gtag, css: "main.css" };
+		main.initialize(options).then(() => import("./app.js"));
 	}
 );
